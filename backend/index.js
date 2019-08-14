988d55cb38dc93d98b0eba591d1f211d46b32d8a
Michael Fornaro <Michael.Fornaro@anz.com>
2019-08-14 11:02:53 +1000

    update frontend forms, add CORS support on backend

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,25 @@ const client = new plivo.Client(process.env.AUTH_ID, process.env.AUTH_TOKEN);
 const port = process.env.PORT || 8081
 
 app.post('/send', async (req, res) => {
-    let message = await client.messages.create(
-        req.body.source,
-        req.body.destination,
-        req.body.message
-    )
-    res.send(`Message Sent: ${message}`)
+    if (req.method === 'OPTIONS') {
+        // Set CORS headers for preflight requests
+        // Allows GETs from any origin with the Content-Type header
+        // and caches preflight response for 3600s
+        // Send response to OPTIONS requests
+        response.set('Access-Control-Allow-Methods', 'POST');
+        response.set('Access-Control-Allow-Headers', 'Content-Type');
+        response.set('Access-Control-Max-Age', '3600');
+        response.status(204).send('');
+    } else {
+        console.log(req.body)
+        let message = await client.messages.create(
+            req.body.source,
+            req.body.destination,
+            req.body.message
+        )
+        res.set('Access-Control-Allow-Origin', '*')
+        res.send(`Message Sent: ${message}`)
+    }
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+export default class App extends Component {
+
+  state = {
+    fields: {}
+  }
+
+  onChange = updatedValue => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        ...updatedValue
+      }
+    })
+  }
+
+  onSubmit = fields => {
+    axios.post('http://localhost:8081/send', fields)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Try sending a message!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </header>
+        <Form onSubmit={fields => this.onSubmit(fields)} onChange={fields => this.onChange(fields)} />
+        <p>
+          {JSON.stringify(this.state.fields, null, 2)}
+        </p>
+      </div>
+    );
+  }
 }
-
-export default App;
